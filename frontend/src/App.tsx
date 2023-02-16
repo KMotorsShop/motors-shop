c0d2419a8221097e3d6225f929683f0ac09c0c93
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-16 15:39:54 -0300

    feat: removed chakra, removed .css file imports from app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { CardFooter, Button, IconButton } from "@chakra-ui/react";
-
 import { Footer } from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
