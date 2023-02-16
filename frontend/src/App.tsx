4e36d17b80367ef30dc1dbbbccc0329556f95731
Arthur Ribeiro Goncalves <ribeirogon.arthur@gmail.com>
2023-02-15 22:52:40 -0300

    fix: CardProduct removido da tela inicial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <div className="App">
-      <CardProduct></CardProduct>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
