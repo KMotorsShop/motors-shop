0f1c884b9a74f9637c4946efe09f8bfe1aabeadc
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-22 15:00:47 -0300

    fix: typeorm configuration

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,36 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
-import "dotenv/config"
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
-
-export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: async (): Promise<TypeOrmModuleOptions> => {
-
-    return {
-      type: 'postgres',
-      host: process.env.HOST,
-      port: parseInt(process.env.POSTGRES_PORT as string),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [__dirname + '/../**/*.entity.{js,ts}'],
-      migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-      // cli: {
-      //   migrationsDir: __dirname + '/../migrations',
-      // },
-      extra: {
-        charset: 'utf8mb4_unicode_ci',
-      },
-      synchronize: true,
-      logging: true,
-    } 
-  }
-};
-
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const typeOrmConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.HOST,
   port: parseInt(process.env.POSTGRES_PORT as string),
@@ -39,12 +11,13 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: process.env.POSTGRES_DB,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-  // cli: {
-  //   migrationsDir: __dirname + '/../migrations',
-  // },
   extra: {
     charset: 'utf8mb4_unicode_ci',
   },
   synchronize: true,
   logging: true,
-}
+};
+
+const datasource = new DataSource(typeOrmConfig);
+
+export default datasource;
