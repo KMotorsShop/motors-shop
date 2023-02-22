d53826398416f439ef060bad3ae325521fde39bf
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-22 15:55:45 -0300

    feat: Ad request data validation

--- backend/src/ads/dto/create-ad.dto.ts
+++ backend/src/ads/dto/create-ad.dto.ts
@@ -1,1 +1,26 @@
-export class CreateAdDto {}
+import { IsDefined, Max, MaxLength, Min } from 'class-validator';
+
+export class CreateAdDto {
+  @Min(1000)
+  @Max(9999)
+  @IsDefined()
+  year: number;
+
+  @IsDefined()
+  @Min(1)
+  price: number;
+
+  @IsDefined()
+  @MaxLength(64)
+  name: string;
+
+  @IsDefined()
+  type: string;
+
+  @IsDefined()
+  @Min(0)
+  km: number;
+
+  @IsDefined()
+  description: string;
+}
