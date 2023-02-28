0eba0f46e2627961dffdbc49a9c183a62168dc4c
Micaías Silva <micaiasilvaop@gmail.com>
2023-02-28 15:21:04 -0300

    feat: implementation of createAnnounceForm in a modal

--- frontend/src/components/CreateAnnounceModal/index.tsx
+++ frontend/src/components/CreateAnnounceModal/index.tsx
@@ -1,17 +1,48 @@
 import AnnounceForm from "../AnnounceForm";
 import * as yup from "yup";
 import api from "../../services/api";
+import ReactModal from "react-modal";
+import { useEffect, useState } from "react";
+import BasicModal from "../BasicModal";
+import { BrandButton, OutlineButton } from "../../styles/Buttons";
+import { ModalStatusMessage } from "../../styles/Containers";
+import { Heading7, Text } from "../../styles/Texts";
+import { fullParseInt } from "../../tools/formatters";
 
 const CreateAnnounceModal = () => {
+  const [modalIsOpen, setModalOpen] = useState<boolean>(false);
+  const [announceWasCreated, setAnnounceCreated] = useState(false);
+  const [title, setTitle] = useState<string>("");
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => {
+    setModalOpen(false);
+    setAnnounceCreated(false);
+  };
+
+  useEffect(() => {
+    if (announceWasCreated) {
+      setTitle("Sucesso!");
+    } else {
+      setTitle("Criar anúncio");
+    }
+  }, [announceWasCreated]);
+
+  const today = new Date(Date.now());
+
   const createAnnounceSchema = yup.object().shape({
     name: yup.string().required("Campo obrigatório"),
-    year: yup.number().typeError("Ano inválido").required("Campo obrigatório"),
+    year: yup
+      .number()
+      .typeError("Ano inválido")
+      .min(1885, "Ano inválido")
+      .max(today.getFullYear(), "Ano inválido")
+      .required("Campo obrigatório"),
     km: yup
       .number()
       .typeError("Quilometragem Inválida")
       .required("Campo obrigatório"),
     price: yup
-      .number()
+      .string()
       .typeError("Preço inválido")
       .required("Campo Obrigatório"),
     description: yup.string().required("Campo obrigatório"),
@@ -32,9 +63,8 @@ const CreateAnnounceModal = () => {
     img5: yup.string(),
     img6: yup.string(),
   });
-  const onSubmitFunction = (data: any) => {
-    console.log(data);
 
+  const onSubmitFunction = (data: any) => {
     const images = [
       data.capeImage,
       data.img1,
@@ -43,25 +73,50 @@ const CreateAnnounceModal = () => {
       data.img4,
       data.img5,
       data.img6,
-    ];
+    ].filter((img) => img != undefined);
 
     const sendData = {
       name: data.name,
       year: data.year,
       km: data.km,
-      price: data.price,
+      price: fullParseInt(data.price),
       description: data.description,
       type: data.type,
       images,
     };
 
-    api.post("/ads", sendData).then((res) => console.log(res));
+    api.post("/ads", sendData).then((res) => {
+      res.status === 201 ? setAnnounceCreated(true) : null;
+    });
   };
+
   return (
-    <AnnounceForm
-      schema={createAnnounceSchema}
-      onSubmitFunction={onSubmitFunction}
-    />
+    <div>
+      <OutlineButton variant="brand" onClick={openModal}>
+        Criar anúncio
+      </OutlineButton>
+      <BasicModal
+        title={title}
+        isOpen={modalIsOpen}
+        openModal={openModal}
+        closeModal={closeModal}
+      >
+        {announceWasCreated ? (
+          <ModalStatusMessage>
+            <Heading7>Seu anúncio foi criado com sucesso!</Heading7>
+            <Text color="grey2">
+              Agora você poderá ver seus negócios crescendo em grande escala
+            </Text>
+          </ModalStatusMessage>
+        ) : (
+          <AnnounceForm
+            schema={createAnnounceSchema}
+            onSubmitFunction={onSubmitFunction}
+            closeContainingModal={closeModal}
+          />
+        )}
+      </BasicModal>
+    </div>
   );
 };
 
