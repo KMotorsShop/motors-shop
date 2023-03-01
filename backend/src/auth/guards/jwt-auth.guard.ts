a707bf388c9261d9228be65e72bb48a4b74365a2
Arthur Ribeiro Goncalves <ribeirogon.arthur@gmail.com>
2023-03-01 13:36:02 -0300

    Fix: Unauthorized creation and importion made.

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -9,6 +9,7 @@ import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 // Decorators
 import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
+import { UnauthorizedError } from '../errors/unauthorized.error';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
