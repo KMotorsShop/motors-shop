2fc9f0117d84aa57b06aa8524d7cc9de3d55fc2d
Micaias Silva <micaiasilvaop@gmail.com>
2023-03-08 18:34:07 -0300

    feat: changed props implementation

--- frontend/src/components/ProductSection/index.tsx
+++ frontend/src/components/ProductSection/index.tsx
@@ -31,17 +31,7 @@ export const ProductSection = ({ type }: ProductSectionProps) => {
       <List>
         {vehicles.map((product, index) => {
           if (product.type === type) {
-            return (
-              <CardProduct
-                id={product.id}
-                key={index}
-                name={product.name}
-                description={product.description}
-                km={product.km}
-                price={product.price}
-                year={product.year}
-              ></CardProduct>
-            );
+            return <CardProduct key={index} {...product}></CardProduct>;
           }
         })}
       </List>
