01ad738440d654dbd5cd65f704fda3bc3b545d48
Micaias Silva <micaiasilvaop@gmail.com>
2023-03-08 18:33:21 -0300

    feat: changed cursor on UserLogo

--- frontend/src/components/Comment/styles.ts
+++ frontend/src/components/Comment/styles.ts
@@ -1,37 +1,38 @@
 import { styled } from "../../styles/stitches.config";
 
-export const Container = styled("section", { 
-    maxHeight: "100%",
-    minWidth: "100%",
-    padding: "20px",
-    top: "256px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "10px",
+export const Container = styled("section", {
+  maxHeight: "100%",
+  minWidth: "100%",
+  padding: "20px",
+  top: "256px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "10px",
 });
 
 export const UserLogo = styled("span", {
-    height: "32px",
-    width: "32px",
-    borderRadius: "150px",
-    background: "#7000FF",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "white",
+  cursor: "default",
+  height: "32px",
+  width: "32px",
+  borderRadius: "150px",
+  background: "#7000FF",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  color: "white",
 });
 
 export const CommentTime = styled("span", {
-    color: "#868E96",
-    fontSize: "14px",
-})
+  color: "#868E96",
+  fontSize: "14px",
+});
 
 export const UserInfos = styled("div", {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    p: {
-        fontSize: "14px",
-        fontWeight: "bold",
-    },
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  p: {
+    fontSize: "14px",
+    fontWeight: "bold",
+  },
 });
